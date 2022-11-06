d83e93387963c481bdbf998fa0c5ebaacc39c51e
LeeJungHwan-Dev <aftermoa@gmail.com>
2022-11-06 22:47:25 +0900

    init project

---
+++ index.js
@@ -0,0 +1,24 @@
+const { RTMClient } = require('@slack/rtm-api');
+const greeting = require('./module/greeting');
+const token = require('./module/token');
+const square = require('./module/square');
+
+const rtm = new RTMClient(token);
+rtm.start();
+
+rtm.on('message', (message) => {
+  const { channel } = message;
+  const { text } = message;
+
+  if (!Number.isNaN(text)) {
+    square(rtm, text, channel);
+  } else {
+    switch (text) {
+      case 'hi':
+        greeting(rtm, channel);
+        break;
+      default:
+        rtm.sendMessage(" I'm alive", channel);
+    }
+  }
+});
---
+++ module/token.js
@@ -0,0 +1,3 @@
+const token = 'xoxb-4214668534118-4240518786023-GZCwFkTQkHo4BUEqCc2ujsir';
+
+module.exports = token;
