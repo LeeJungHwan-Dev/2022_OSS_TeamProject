9dc42e7c20c6c7d283c3dba3445d6933f4659b6f
Yasasayi <bestezrealkr@naver.com>
2022-11-11 12:43:15 +0900

    mocha test

---
+++ hello.spec.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+
+const { RTMClient } = require('@slack/rtm-api');
+const fs = require('fs');
+
+const channel = 'D047VL6CF09';
+
+let token;
+
+try {
+  token = fs.readFileSync('./token').toString('utf-8');
+} catch (err) {
+  console.error(err);
+}
+
+console.log(token);
+
+const rtm = new RTMClient(token);
+
+(async () => {
+  await rtm.start().catch(console.error);
+})();
+
+const assert = require('assert');
+const greeting = require('./module/greeting');
+
+let res;
+
+describe('테스트를 시작합니다.', async () => {
+  // eslint-disable-next-line no-undef
+  before(async () => {
+    await greeting(rtm, channel);
+  });
+
+  it('먼저 모듈 테스트', (done) => {
+    console.log(res);
+    assert.equal(res, 'success');
+    done();
+  });
+});
