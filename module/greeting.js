0d42dad17b45805ae0823cc64fa500bf6cd974e5
Yasasayi <bestezrealkr@naver.com>
2022-11-18 12:05:13 +0900

    fix index.js

--- module/greeting.js
+++ module/greeting.js
@@ -1,15 +1,13 @@
 const greeting = function (rtm, channel) {
   console.log('인사를 합니다.');
 
-  const rand = Math.random() * 3 + 1; // 1 ~ 3의 난수 생성
+  const rand = Math.floor(Math.random() * 3) + 1; // 1 ~ 3의 난수 생성
 
-  rtm.sendMessage(String(rand), channel);
-
-  if (Math.floor(rand) === 1) {
+  if (rand === 1) {
     rtm.sendMessage('Hello', channel);
-  } else if (Math.floor(rand) === 2) {
+  } else if (rand === 2) {
     rtm.sendMessage('Hi', channel);
-  } else if (Math.floor(rand) === 3) {
+  } else if (rand === 3) {
     rtm.sendMessage('Bonjour', channel);
   } else {
     rtm.sendMessage('Error', channel);
