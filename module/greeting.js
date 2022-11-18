17bf5000d3d062a00e97fc488dfbc35c50b925c0
Yasasayi <bestezrealkr@naver.com>
2022-11-18 11:36:59 +0900

    first

--- module/greeting.js
+++ module/greeting.js
@@ -1,6 +1,17 @@
 const greeting = function (rtm, channel) {
   console.log('인사를 합니다.');
-  rtm.sendMessage('Hello', channel);
+
+  const rand = Math.floor(Math.random() * 3) + 1; // 1 ~ 3의 난수 생성
+
+  if (rand === 1) {
+    rtm.sendMessage('Hello', channel);
+  } else if (rand === 2) {
+    rtm.sendMessage('Hi', channel);
+  } else if (rand === 3) {
+    rtm.sendMessage('Bonjour', channel);
+  } else {
+    rtm.sendMessage('Error', channel);
+  }
 };
 
 module.exports = greeting;
