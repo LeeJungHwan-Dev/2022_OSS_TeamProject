003a8df3009851c71241d4a9c3e61a533cbf5084
Yasasayi <bestezrealkr@naver.com>
2022-11-18 11:32:34 +0900

    Revert "first"
    This reverts commit c1080d09ffe7ab41a8538940a407f555faf13d96.

--- module/greeting.js
+++ module/greeting.js
@@ -1,17 +1,6 @@
 const greeting = function (rtm, channel) {
   console.log('인사를 합니다.');
-
-  const rand = Math.floor(Math.random() * 3) + 1; // 1 ~ 3의 난수 생성
-
-  if (rand === 1) {
-    rtm.sendMessage('Hello', channel);
-  } else if (rand === 2) {
-    rtm.sendMessage('Hi', channel);
-  } else if (rand === 3) {
-    rtm.sendMessage('Bonjour', channel);
-  } else {
-    rtm.sendMessage('Error', channel);
-  }
+  rtm.sendMessage('Hello', channel);
 };
 
 module.exports = greeting;
