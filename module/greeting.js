8e9739336ebc03d28946a88e78fc17b8afec4796
Yasasayi <bestezrealkr@naver.com>
2022-11-21 20:37:43 +0900

    ver 0.1.5

--- module/greeting.js
+++ module/greeting.js
@@ -7,6 +7,7 @@ const greeting = function (rtm, channel) {
     rtm.sendMessage(
       `안녕하세요? 전북대학교 챗봇입니다.
 다음의 기능들을 제공하고 있습니다.
+
 1. 학사일정
 2. 식단 조회
 3. 학과 사무실 조회`,
@@ -16,6 +17,7 @@ const greeting = function (rtm, channel) {
     rtm.sendMessage(
       `Hello? I'm Chonbuk National University chatbot.
 I provide the following features.
+
 1. Academic Calendar
 2. Check Menu
 3. Check Office of Academic Department`,
@@ -25,6 +27,7 @@ I provide the following features.
     rtm.sendMessage(
       `こんにちは？全北大学チャットボットです。
 以下の機能を提供しています。
+
 1. 学士スケジュール
 2. 食事の検索
 3. 学科事務所検索`,
