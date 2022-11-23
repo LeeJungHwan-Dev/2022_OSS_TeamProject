0468db32a11b175322b17985d375d198e9c1d958
happywood007 <113480966+happywood007@users.noreply.github.com>
2022-11-24 00:42:22 +0900

    Merge Feature #3 Update ver 0.1.3

--- module/greeting.js
+++ module/greeting.js
@@ -1,5 +1,5 @@
 const greeting = function (rtm, channel) {
-  console.log('인사를 합니다.');
+  console.log("인사를 합니다.");
 
   const rand = Math.floor(Math.random() * 3) + 1; // 1 ~ 3의 난수 생성
 
@@ -11,7 +11,7 @@ const greeting = function (rtm, channel) {
 1. 학사일정
 2. 식단 조회
 3. 학과 사무실 조회`,
-      channel,
+      channel
     );
   } else if (rand === 2) {
     rtm.sendMessage(
@@ -21,7 +21,7 @@ I provide the following features.
 1. Academic Calendar
 2. Check Menu
 3. Check Office of Academic Department`,
-      channel,
+      channel
     );
   } else if (rand === 3) {
     rtm.sendMessage(
@@ -31,10 +31,10 @@ I provide the following features.
 1. 学士スケジュール
 2. 食事の検索
 3. 学科事務所検索`,
-      channel,
+      channel
     );
   } else {
-    rtm.sendMessage('Error of random()', channel);
+    rtm.sendMessage("Error of random()", channel);
   }
 };
 
