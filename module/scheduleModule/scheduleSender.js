76adb5113fbbfeef660b027f8a1d9de661824d96
JungHwan_Lee <93726941+LeeJungHwan-Dev@users.noreply.github.com>
2022-11-24 03:31:29 +0900

    Bug Fix : feature #2 Date Formate bug fix

--- module/scheduleModule/scheduleSender.js
+++ module/scheduleModule/scheduleSender.js
@@ -1,22 +1,17 @@
 const check = require('../seletedCheckModule');
 const schedule = require('./indexIngSchedule');
+const getPattern = require('./patternChecker');
 
 let keyList;
 // index.js에서 생성한 key값을 담을 리스트 변수
 
-const pattern = /^([1-9]|1[012])\/([1-9]|[12][0-9]|3[0-1])$/;
-// mm/dd 패턴 정규식
-const pattern2 =
-  /^([1-9]|1[012])\/([1-9]|[12][0-9]|3[0-1])-([1-9]|1[012])\/([1-9]|[12][0-9]|3[0-1])$/;
-// mm/dd - mm/dd 패턴 정규식
-
-function scheduleListSender(dateList, rtm, channel) {
-  let keyindex = keyList.indexOf(dateList[0]);
+function scheduleListSender(firstdate, lastdate, rtm, channel) {
+  let keyindex = keyList.indexOf(firstdate);
   // keyindex는 사용자가 입력한 첫째날 인덱스부터 더해질 변수이다.
   while (true) {
     if (
       keyList.at(keyindex) === undefined ||
-      keyList.at(keyindex) === dateList[1]
+      keyList.at(keyindex) === lastdate
     ) {
       // 날짜가 더이상 존재하지 않거나, 입력한 마지막 날짜와 같을경우 break를 위한 if문
       rtm.sendMessage(
@@ -46,8 +41,14 @@ function scheduleListSender(dateList, rtm, channel) {
 function noScheduleCheck(removeBlankDate, rtm, channel) {
   keyList = Object.keys(schedule.scheduleList);
   const dateList = removeBlankDate.split('-');
-  const keyindex1 = keyList.indexOf(dateList[0]);
-  const keyindex2 = keyList.indexOf(dateList[1]);
+  const firstdate = new Date(dateList[0]);
+  const lastdate = new Date(dateList[1]);
+  const keyindex1 = keyList.indexOf(
+    `${firstdate.getMonth() + 1}/${firstdate.getDate()}`,
+  );
+  const keyindex2 = keyList.indexOf(
+    `${lastdate.getMonth() + 1}/${lastdate.getDate()}`,
+  );
 
   /**
    * 각 keyindex는 key값이 존재하는 위치의 인덱스 값을 담은 변수이다.
@@ -58,7 +59,12 @@ function noScheduleCheck(removeBlankDate, rtm, channel) {
     // 날짜를 9/7 - 9/1 과 같이 범위 설정을 잘 못 하였을 때를 처리하기 위한 if문
     if (keyindex1 !== -1 && keyindex2 !== -1) {
       // 키값에 등록된 올바른 범위일경우
-      scheduleListSender(dateList, rtm, channel);
+      scheduleListSender(
+        `${firstdate.getMonth() + 1}/${firstdate.getDate()}`,
+        `${lastdate.getMonth() + 1}/${lastdate.getDate()}`,
+        rtm,
+        channel,
+      );
     } else {
       // 일정이 둘다 없을경우
       rtm.sendMessage('학사일정이 존재하지 않습니다.', channel);
@@ -75,7 +81,7 @@ function noScheduleCheck(removeBlankDate, rtm, channel) {
 function scheduleSender(date, rtm, channel) {
   const removeBlankDate = date.replace(/(\s*)/g, '');
   // 맨 처음 들어오는 날짜 문자열에서 공백을 모두 제거한다.
-  if (pattern.test(removeBlankDate)) {
+  if (getPattern.getStatus(removeBlankDate) === 1) {
     // 단일 날짜 입력에 따른 패턴 검사
     let resultDate = new Date(`0000${date}`);
     resultDate = `${resultDate.getMonth() + 1}/${resultDate.getDate()}`;
@@ -94,7 +100,7 @@ function scheduleSender(date, rtm, channel) {
       check.setDateCheck(false);
       // 사용자에게 key와 value가 존재할 경우 존재하는 일정을 전달한다.
     }
-  } else if (pattern2.test(removeBlankDate)) {
+  } else if (getPattern.getStatus(removeBlankDate) === 2) {
     // 단일 날짜가 아닌 범위날짜일 경우
     // 스케줄이 범위 내에 있는지 검사하는 함수로 이동한다.
     noScheduleCheck(removeBlankDate, rtm, channel);
