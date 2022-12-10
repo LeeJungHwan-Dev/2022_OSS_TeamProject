aceeacf81713a407fe612e78bb6f70c3b25c7808
happywood007 <113480966+happywood007@users.noreply.github.com>
2022-12-11 02:17:52 +0900

    Update feature #3 version1.2.1 : Merge Star

--- module/getMenuModule/infoFood.js
+++ module/getMenuModule/infoFood.js
@@ -1,5 +1,8 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const star = require('./module/getStarModule/getStart');
+
+star.editStar();
 
 async function webScraping(url, selector) {
   const res = [];
@@ -16,7 +19,7 @@ async function webScraping(url, selector) {
 
 const url = 'https://sobi.jbnu.ac.kr/menu/week_menu.php';
 
-let str;
+let str,starRankWeek,StarRankDay;
 
 const selector = 'font';
 const selector2 = 'span';
@@ -33,13 +36,17 @@ const infoFoodDay = async function (rtm, data, channel) {
   } else if (data === 1) {
     str = `오늘의 메뉴는 '${res2[6]} / ${res2[7]} / ${res2[8]} / ${res2[9]}'입니다.`;
     console.log(str);
+	getStarRank(data);
     rtm.sendMessage(str, channel);
+	rtm.sendMessage(starRankDay,channel);
   } else {
     str = `오늘의 메뉴는 '${res[data * 4 - 7]} / ${res[data * 4 - 6]} / ${
       res[data * 4 - 5]
     } / ${res[data * 4 - 4]}'입니다.`;
     console.log(str);
+	getStarRank(data);
     rtm.sendMessage(str, channel);
+	rtm.sendMessage(starRankDay, channel);
   }
 };
 
@@ -64,7 +71,28 @@ const infoFoodWeek = async function (rtm, channel) {
     console.log(str);
     rtm.sendMessage(str, channel);
   }
+	rtm.sendMessage("월 : "+ star.getStar()[1], channel);
+	rtm.sendMessage("화 : "+ star.getStar()[2], channel);
+	rtm.sendMessage("수 : "+ star.getStar()[3], channel);
+	rtm.sendMessage("목 : "+ star.getStar()[4], channel);
+	rtm.sendMessage("금 : "+ star.getStar()[5], channel);
+	console.log(star.getStar()[1]);
 };
+
+function getStarRank(date){
+	if(date === 1){
+	   starRankDay = "월 : "+ star.getStar()[1];
+	}else if(date === 2){
+	   console.log('d');
+	   starRankDay = "화 : "+ star.getStar()[2];
+	}else if(date === 3){
+	   starRankDay = "수 : "+ star.getStar()[3];
+	}else if(date === 4){
+	   starRankDay = "목 : "+ star.getStar()[4];
+	}else{
+	   starRankDay = "금 : "+ star.getStar()[5];
+	}
+}
 
 module.exports.infoFoodDay = infoFoodDay;
 module.exports.infoFoodWeek = infoFoodWeek;
